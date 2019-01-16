7353ef372dd884159bb406052b67eaa82d132bba
Marko Kosunen <marko.kosunen@aalto.fi>
2019-01-16 15:35:10 -0800

    Release v1.1 ready

--- verilog/__init__.py
+++ verilog/__init__.py
@@ -18,13 +18,13 @@ from functools import reduce
 class verilog_iofile(thesdk):
     def __init__(self,parent=None,**kwargs):
         if parent==None:
-            self.print_log({'type':'F', 'msg':"Parent of Verilog input file not given"})
+            self.print_log(type='F', msg="Parent of Verilog input file not given")
         try:  
             rndpart=os.path.basename(tempfile.mkstemp()[1])
             self.name=kwargs.get('name') 
             self.file=parent.vlogsimpath +'/' + self.name + '_' + rndpart +'.txt'
         except:
-            self.print_log({'type':'F', 'msg':"Verilog IO file definition failed"})
+            self.print_log(type='F', msg="Verilog IO file definition failed")
 
         self.data=kwargs.get('data',[])
         self.simparam=kwargs.get('param','-g g_file_' + kwargs.get('name') + '=' + self.file)
@@ -41,11 +41,11 @@ class verilog_iofile(thesdk):
             self.preserve=False
 
         #TODO: Needs a check to eliminate duplicate entries to iofiles
-        parent.iofiles.append(self)
-        self.print_log({'type':'O','msg':"Attribute iofiles has been replaced by _iofile_bundle.\nSupport will be removed in the future"})
+        if hasattr(parent,'iofiles'):
+            self.print_log(type='O',msg="Attribute iofiles has been replaced by iofile_bundle")
 
-        if hasattr(parent,'_iofile_bundle'):
-            parent._iofile_bundle.new(name=self.name,val=self)
+        if hasattr(parent,'iofile_bundle'):
+            parent.iofile_bundle.new(name=self.name,val=self)
 
     @property
     def verilog_stat(self):
@@ -186,7 +186,6 @@ class verilog_iofile(thesdk):
 
             df=pd.DataFrame(parsed,dtype=datatype)
             if self.hasheader:
-                print(self.hasheader)
                 df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=header_line)
             else:
                 df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=False)
@@ -194,7 +193,7 @@ class verilog_iofile(thesdk):
             for i in range(data.shape[1]):
                 if i==0:
                    if np.iscomplex(data[0,i]) or np.iscomplexobj(data[0,i]) :
-                       self.print_log({'type':'F', 'msg':'Timestamp can not be complex.'})
+                       self.print_log(type='F', msg='Timestamp can not be complex.')
                    else:
                        parsed=np.r_['1',data[:,i].reshape(-1,1)]
                        header_line.append('Timestamp')
@@ -209,7 +208,6 @@ class verilog_iofile(thesdk):
 
             df=pd.DataFrame(parsed,dtype=datatype)
             if self.hasheader:
-                print(self.hasheader)
                 df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=header_line)
             else:
                 df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=False)
@@ -224,8 +222,8 @@ class verilog_iofile(thesdk):
 
     def remove(self):
         if self.preserve:
-            self.print_log({'type':"I", 'msg':"Preserve_value is %s" %(self.preserve)})
-            self.print_log({'type':"I", 'msg':"Preserving file %s" %(self.file)})
+            self.print_log(type="I", msg="Preserve_value is %s" %(self.preserve))
+            self.print_log(type="I", msg="Preserving file %s" %(self.file))
         else:
             try:
                 os.remove(self.file)
@@ -270,36 +268,36 @@ class verilog(thesdk,metaclass=abc.ABCMeta):
     def interactive_verilog(self,value):
         self._interactive_verilog=value
     
-    # This property utilises verilog_iofile class to maintain list of io-files
-    # that  are automatically assigned to vlogcmd
+    # This property utilises vhdl_iofile class to maintain list of io-files
+    # that  are automatically assigned to vhdlcmd
     @property
-    def iofiles(self):
-        if hasattr(self,'_iofiles'):
-            return self._iofiles
-        else:
-            self._iofiles=list([])
-            return self._iofiles
+    def iofile_bundle(self):
+        if not hasattr(self,'_iofile_bundle'):
+            self._iofile_bundle=Bundle()
+        return self._iofile_bundle
 
-    @iofiles.setter
-    def iofiles(self,value):
-        self._iofiles=list[value]
+    @iofile_bundle.setter
+    def iofile_bundle(self,value):
+        self._iofile_bundle=value
 
-    @iofiles.deleter
-    def iofiles(self):
-        for i in self.iofiles:
-            if i.preserve:
-                self.print_log({'type':"I", 'msg':"Preserve_value is %s" %(i.preserve)})
-                self.print_log({'type':"I", 'msg':"Preserving file %s" %(i.file)})
+    @iofile_bundle.deleter
+    def iofile_bundle(self):
+        for name, val in self.iofile_bundle.Members.items():
+            if val.preserve:
+                self.print_log(type="I", msg="Preserve_value is %s" %(val.preserve))
+                self.print_log(type="I", msg="Preserving file %s" %(val.file))
             else:
-                i.remove()
-                self._iofiles=None
+                val.remove()
+                _=self.iofile_bundle.Members.pop(name)
+        self._iofile_bundle=None
+
     @property 
     def verilog_submission(self):
         if not hasattr(self, '_verilog_submission'):
             try:
                 self._verilog_submission=thesdk.GLOBALS['LSFSUBMISSION']+' '
             except:
-                self.print_log({'type':'W','msg':'Variable thesdk.GLOBALS incorrectly defined. Implemented in thesdk module, commit  04a3c519eeed995121d764762432ce48b9e1d0f6 _verilog_submission defaults to empty string and simulation is ran in localhost.'})
+                self.print_log(type='W',msg='Variable thesdk.GLOBALS incorrectly defined. _verilog_submission defaults to empty string and simulation is ran in localhost.')
                 self._verilog_submission=''
 
         if hasattr(self,'_interactive_verilog'):
@@ -375,7 +373,7 @@ class verilog(thesdk,metaclass=abc.ABCMeta):
 
     #This is obsoleted
     def def_verilog(self):
-        self.print_log({'type':'I','msg':'Command def_verilog() is obsoleted. It does nothing. \nWill be removed in future releases'})
+        self.print_log(type='I',msg='Command def_verilog() is obsoleted. It does nothing. \nWill be removed in future releases')
 
     @property
     def vlogcmd(self):
@@ -388,41 +386,20 @@ class verilog(thesdk,metaclass=abc.ABCMeta):
                            + self.vlogsrcpath + '/tb_' + self.name +'.sv' + ' ' + vlogmodulesstring )
 
             gstring=' '.join([ ('-g ' + str(param) +'='+ str(val)) for param,val in iter(self.vlogparameters.items()) ])
-            
-            if hasattr(self,'_infile') or hasattr(self,'_outfile'):
-                self.print_log({'type':'W', 'msg':'OBSOLETE CODE: _infile and _outfile properties are\n'                    
-                    +'replaced by iofiles property enabling multiple files and '
-                    +'automating the definitions. Use that instead. Will be removed in Jan 2019'})
-                if not self.interactive_verilog:
-                    vlogsimcmd = ( 'vsim -64 -batch -t 1ps -voptargs=+acc -g g_infile=' + self._infile
-                              + ' -g g_outfile=' + self._outfile + ' ' + gstring 
-                              +' work.tb_' + self.name  + ' -do "run -all; quit;"')
-                else:
-                    submission="" #Local execution
-                    vlogsimcmd = ( 'vsim -64 -t 1ps -novopt -g g_infile=' + self._infile
-                              + ' -g g_outfile=' + self._outfile + ' ' + gstring 
-                              +' work.tb_' + self.name)
 
-            elif ( not ( hasattr(self,'_infile') or  hasattr(self,'_outfile') )): 
-                if hasattr(self,'iofiles'):
-                    fileparams=''
-                    for file in self.iofiles:
-                        fileparams=fileparams+' '+file.simparam
+            fileparams=''
+            for name, file in self.iofile_bundle.Members.items():
+                fileparams=fileparams+' '+file.simparam
 
-                if hasattr(self,'_iofile_bundle'):
-                    fileparams=''
-                    for name, file in self._iofile_bundle.Members.items():
-                        fileparams=fileparams+' '+file.simparam
+            if not self.interactive_verilog:
+                vlogsimcmd = ( 'vsim -64 -batch -t 1ps -voptargs=+acc ' + fileparams + ' ' + gstring
+                          +' work.tb_' + self.name  + ' -do "run -all; quit;"')
+            else:
+                submission="" #Local execution
+                vlogsimcmd = ( 'vsim -64 -t 1ps -novopt ' + fileparams + ' ' + gstring
+                          +' work.tb_' + self.name)
 
-                if not self.interactive_verilog:
-                    vlogsimcmd = ( 'vsim -64 -batch -t 1ps -voptargs=+acc ' + fileparams + ' ' + gstring
-                              +' work.tb_' + self.name  + ' -do "run -all; quit;"')
-                else:
-                    submission="" #Local execution
-                    vlogsimcmd = ( 'vsim -64 -t 1ps -novopt ' + fileparams + ' ' + gstring
-                              +' work.tb_' + self.name)
-
-            self._vlogcmd =  submission + vloglibcmd  +  ' && ' + vloglibmapcmd + ' && ' + vlogcompcmd +  ' && ' + vlogsimcmd
+            self._vlogcmd =  vloglibcmd  +  ' && ' + vloglibmapcmd + ' && ' + vlogcompcmd +  ' && ' + submission + vlogsimcmd
         return self._vlogcmd
     # Just to give the freedom to set this if needed
     @vlogcmd.setter
@@ -435,71 +412,51 @@ class verilog(thesdk,metaclass=abc.ABCMeta):
     def run_verilog(self):
         filetimeout=60 #File appearance timeout in seconds
         count=0
-        #This is to ensure operation of obsoleted code, to be removed
-        if hasattr(self,'_infile'):
-            while not os.path.isfile(self._infile):
-                count +=1
-                if count >5:
-                    self.print_log({'type':'F', 'msg':"Verilog infile writing timeout"})
-                time.sleep(int(filetimeout/5))
-        else:
-            files_ok=False
-            while not files_ok:
-                count +=1
-                if count >5:
-                    self.print_log({'type':'F', 'msg':"Verilog infile writing timeout"})
-                for file in list(filter(lambda x:x.dir=='in',self.iofiles)):
+        files_ok=False
+        while not files_ok:
+            count +=1
+            if count >5:
+                self.print_log(type='F', msg="Verilog infile writing timeout")
+            for name, file in self.iofile_bundle.Members.items(): 
+                if file.dir=='in':
                     files_ok=True
                     files_ok=files_ok and os.path.isfile(file.file)
-                time.sleep(int(filetimeout/5))
+            time.sleep(int(filetimeout/5))
 
         #Remove existing output files before execution
-        if hasattr(self,'_outfile'):
-            try:
-                os.remove(self._outfile)
-            except:
-                pass
-        else:
-            for file in list(filter(lambda x:x.dir=='out',self.iofiles)):
+        for name, file in self.iofile_bundle.Members.items(): 
+            if file.dir=='out':
                 try:
                     #Still keep the file in the infiles list
                     os.remove(file.name)
                 except:
                     pass
 
-        self.print_log({'type':'I', 'msg':"Running external command %s\n" %(self.vlogcmd) })
+        self.print_log(type='I', msg="Running external command %s\n" %(self.vlogcmd) )
 
         if self.interactive_verilog:
-            self.print_log({'type':'I', 'msg':"""Running verilog simulation in interactive mode.
+            self.print_log(type='I', msg="""Running verilog simulation in interactive mode.
                 Add the probes in the simulation as you wish.
-                To finish the simulation, run the simulation to end and exit."""})
+                To finish the simulation, run the simulation to end and exit.""")
 
         subprocess.check_output(self.vlogcmd, shell=True);
 
         count=0
-        #This is to ensure operation of obsoleted code, to be removed
-        if hasattr(self,'_outfile'):
-            while not os.path.isfile(self._outfile):
-                count +=1
-                if count >5:
-                    self.print_log({'type':'F', 'msg':"Verilog outfile timeout"})
-                time.sleep(int(filetimeout/5))
-            if not self.preserve_iofiles:
-                os.remove(self._infile)
-        else:
-            files_ok=False
-            while not files_ok:
-                count +=1
-                if count >5:
-                    self.print_log({'type':'F', 'msg':"Verilog outfile timeout"})
-                time.sleep(int(filetimeout/5))
-                for file in list(filter(lambda x:x.dir=='out',self.iofiles)):
+        files_ok=False
+        while not files_ok:
+            count +=1
+            if count >5:
+                self.print_log(type='F', msg="Verilog outfile timeout")
+            time.sleep(int(filetimeout/5))
+            for name, file in self.iofile_bundle.Members.items(): 
+                if file.dir=='out':
                     files_ok=True
                     files_ok=files_ok and os.path.isfile(file.file)
 
-            for file in list(filter(lambda x:x.dir=='in',self.iofiles)):
-                try:
-                    file.remove()
-                except:
-                    pass
+        #for name, file in self.iofile_bundle.Members.items(): 
+        #    if file.dir=='in':
+        #        try:
+        #            file.remove()
+        #        except:
+        #            pass
 
