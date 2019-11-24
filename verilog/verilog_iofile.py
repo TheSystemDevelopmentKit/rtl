2844f1cc3cfd40943ffbc70fbb948800b4ae40cb
Marko Kosunen <marko.kosunen@aalto.fi>
2019-11-24 11:20:21 +0200

    Generic operations of verilog_iofile to thesdk.iofile
    Generic file IO-functions are now provided with thesdk.iofile
    package
    
    OBS: This version adn versions onwards require
    commit #49753402039ddd49b
    of thesdk v1.3_RC devel branch, (or later)
    
    [TODO]
    Transfer verilog-specific functionality to verilog_iofile package
    
    subject line
    
    what happened
    
    [ticket: X]

--- verilog/verilog_iofile.py
+++ verilog/verilog_iofile.py
@@ -14,11 +14,12 @@ import os
 import sys
 from abc import * 
 from thesdk import *
+from thesdk.iofile import iofile
 import numpy as np
 import pandas as pd
 from verilog.connector import intend
 
-class verilog_iofile(IO):
+class verilog_iofile(iofile):
     """
     Class to provide file IO for verilog simulations. When created, 
     adds a verilog_iofile object to the parents iofile_bundle attribute.
@@ -44,90 +45,22 @@ class verilog_iofile(IO):
                 passed to the simulator at command line.
     """
     def __init__(self,parent=None,**kwargs):
+        #This is a redundant check, but doens not hurt.to have it here too.
         if parent==None:
             self.print_log(type='F', msg="Parent of Verilog input file not given")
         try:  
-            super(verilog_iofile,self).__init__(**kwargs)
-            self.parent=parent
-            self.rndpart=os.path.basename(tempfile.mkstemp()[1])
-            self.name=kwargs.get('name') 
+            super(verilog_iofile,self).__init__(parent=parent,**kwargs)
             self.paramname=kwargs.get('param','-g g_file_')
 
-            # IO's are output by default
-            # Currently less needed for Python, but used in Verilog
-            self._dir=kwargs.get('dir','out')
-            self._datatype=kwargs.get('datatype','int')
-
-            self._iotype=kwargs.get('iotype','sample') # The file is a data file by default 
-                                                  # Option: sample, event. file 
-                                                  # Events are presented in time-value combinatios 
-                                                  # time in the column 0
-            self._ionames=kwargs.get('ionames',[]) #list of signal names associated with this io
             self._ioformat=kwargs.get('ioformat','%d') #by default, the io values are decimal integer numbers
 
-            self.hasheader=kwargs.get('hasheader',False) # Headers False by default. 
-                                                         # Do not generate things just 
-                                                         # to remove them in the next step
-            if hasattr(self.parent,'preserve_iofiles'):
-                self.preserve=parent.preserve_iofiles
-            else:
-                self.preserve=False
         except:
             self.print_log(type='F', msg="Verilog IO file definition failed")
 
-        #TODO: Needs a check to eliminate duplicate entries to iofiles
-        if hasattr(self.parent,'iofiles'):
-            self.print_log(type='O',msg="Attribute iofiles has been replaced by iofile_bundle")
 
-        if hasattr(self.parent,'iofile_bundle'):
-            self.parent.iofile_bundle.new(name=self.name,val=self)
 
-    # These propertios "extend" IO class, but do not need ot be member of it,
-    # Furthermore IO._Data _must_ me bidirectional. Otherwise driver and target 
-    # Must be defined separately
-    @property  # in | out
-    def dir(self):
-        if hasattr(self,'_dir'):
-            return self._dir
-        else:
-            self._dir=None
-        return self._dir
 
-    @dir.setter
-    def dir(self,value):
-        self._dir=value
 
-    @property
-    def iotype(self):  # sample | event
-        if hasattr(self,'_iotype'):
-            return self._iotype
-        else:
-            self._iotype='sample'
-        return self._iotype
-
-    @property
-    def datatype(self):  # complex | int | scomplex | sint
-        if hasattr(self,'_datatype'):
-            return self._datatype
-        else:
-            self._datatype=None
-        return self._datatype
-
-    @datatype.setter
-    def datatype(self,value):
-        self._datatype=value
-
-    @property
-    def ionames(self): # list of associated verilog ionames
-        if hasattr(self,'_ionames'):
-            return self._ionames
-        else:
-            self._ionames=[]
-        return self._ionames
-
-    @ionames.setter
-    def ionames(self,value):
-        self._ionames=value
 
     @property
     def ioformat(self):
@@ -401,120 +334,4 @@ class verilog_iofile(IO):
         else:
             self.print_log(type='F', msg='Iotype not defined')
         return self._verilog_io
-
-    # Relocate i.e. change parent. 
-    # probably this could be automated
-    # by using properties
-    def adopt(self,parent=None,**kwargs):
-        if parent==None:
-            self.print_log(type='F', msg='Parent must be given for relocation')
-        self.parent=parent
-        if hasattr(self.parent,'iofile_bundle'):
-            self.parent.iofile_bundle.new(name=self.name,val=self)
-
-    # File writing
-    def write(self,**kwargs):
-        self.dir='in'  # Only input files are written
-        #Parse the rows to split complex numbers
-        data=kwargs.get('data',self.Data)
-        datatype=kwargs.get('datatype',self.datatype)
-        iotype=kwargs.get('iotype',self.iotype)
-        header_line = []
-        parsed=[]
-        # Default is the data file
-        if iotype=='sample':
-            for i in range(data.shape[1]):
-                if i==0:
-                   if np.iscomplex(data[0,i]) or np.iscomplexobj(data[0,i]) :
-                       parsed=np.r_['1',np.real(data[:,i]).reshape(-1,1),
-                               np.imag(data[:,i].reshape(-1,1))]
-                       header_line.append('%s_%s_Real' %(self.name,i))
-                       header_line.append('%s_%s_Imag' %(self.name,i))
-                   else:
-                       parsed=np.r_['1',data[:,i].reshape(-1,1)]
-                       header_line.append('%s_%s' %(self.name,i))
-                else:
-                   if np.iscomplex(data[0,i]) or np.iscomplexobj(data[0,i]) :
-                       parsed=np.r_['1',parsed,np.real(data[:,i]).reshape(-1,1),
-                               np.imag(data[:,i].reshape(-1,1))]
-                       header_line.append('%s_%s_Real' %(self.name,i))
-                       header_line.append('%s_%s_Imag' %(self.name,i))
-                   else:
-                       parsed=np.r_['1',parsed,data[:,i].reshape(-1,1)]
-                       header_line.append('%s_%s' %(self.name,i))
-
-            # Numbers are printed as intergers
-            if datatype is [ 'int', 'sint', 'complex', 'scomplex' ]:
-                df=pd.DataFrame(parsed,dtype='int')
-            else:
-                df=pd.DataFrame(parsed,dtype=datatype)
-
-            if self.hasheader:
-                df.to_csv(path_or_buf=self.file,sep="\t",
-                        index=False,header=header_line)
-            else:
-                df.to_csv(path_or_buf=self.file,sep="\t",
-                        index=False,header=False)
-        # Control file is a different thing
-        elif iotype=='event':
-            for i in range(data.shape[1]):
-                if i==0:
-                   if np.iscomplex(data[0,i]) or np.iscomplexobj(data[0,i]) :
-                       self.print_log(type='F', msg='Timestamp can not be complex.')
-                   else:
-                       parsed=np.r_['1',data[:,i].reshape(-1,1)]
-                       header_line.append('Timestamp')
-                else:
-                   if np.iscomplex(data[0,i]) or np.iscomplexobj(data[0,i]) :
-                       parsed=np.r_['1',parsed,np.real(data[:,i]).reshape(-1,1),
-                               np.imag(data[:,i].reshape(-1,1))]
-                       header_line.append('%s_%s_Real' %(self.name,i))
-                       header_line.append('%s_%s_Imag' %(self.name,i))
-                   else:
-                       parsed=np.r_['1',parsed,data[:,i].reshape(-1,1)]
-                       header_line.append('%s_%s' %(self.name,i))
-
-            df=pd.DataFrame(parsed,dtype=datatype)
-            if self.hasheader:
-                df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=header_line)
-            else:
-                df.to_csv(path_or_buf=self.file,sep="\t",index=False,header=False)
-        # This is to compensate filesystem delays
-        time.sleep(10)
-        
-    # Reading
-    def read(self,**kwargs):
-        fid=open(self.file,'r')
-        self.datatype=kwargs.get('datatype',self.datatype)
-        dtype=kwargs.get('dtype',object)
-        readd = pd.read_csv(fid,dtype=dtype,sep='\t',header=None)
-        #read method for complex signal matrix
-        if self.datatype is 'complex' or self.datatype is 'scomplex':
-            self.print_log(type="I", msg="Reading complex")
-            rows=int(readd.values.shape[0])
-            cols=int(readd.values.shape[1]/2)
-            for i in range(cols):
-                if i==0:
-                    self.Data=np.zeros((rows, cols),dtype=complex)
-                    self.Data[:,i]=readd.values[:,2*i].astype('int')\
-                            +1j*readd.values[:,2*i+1].astype('int')
-                else:
-                    self.Data[:,i]=readd.values[:,2*i].astype('int')\
-                            +1j*readd.values[:,2*i+1].astype('int')
-
-        else:
-            self.Data=readd.values
-        fid.close()
-
-    # Remove the file when no longer needed
-    def remove(self):
-        if self.preserve:
-            self.print_log(type="I", msg="Preserve_value is %s" %(self.preserve))
-            self.print_log(type="I", msg="Preserving file %s" %(self.file))
-        else:
-            try:
-                os.remove(self.file)
-            except:
-                pass
-
 
