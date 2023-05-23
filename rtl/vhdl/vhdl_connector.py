552bec134a65be5b3eca142b2e220b17a940e056
Marko Kosunen <marko.kosunen@aalto.fi>
2023-05-23 23:12:14 +0300

    Add vhdl_connector

---
+++ rtl/vhdl/vhdl_connector.py
@@ -0,0 +1,72 @@
+"""
+=================
+Verilog connector
+=================
+Class for describing signals in wide sense, including IO's
+
+Written by Marko Kosunen 20190109 marko.kosunen@aalto.fi
+"""
+import os
+from thesdk import *
+from rtl.connector_common import connector_common
+
+class vhdl_connector(connector_common,thesdk):
+    def __init__(self, **kwargs):
+        ''' Executes init of connector_common, thus having the same attributes and 
+        parameters.
+
+        Parameters
+        ----------
+            **kwargs :
+               See module module_common
+        
+        '''
+        super().__init__(**kwargs)
+
+    @property
+    def ioformat(self):
+        if not hasattr(self, '_ioformat') or self._ioformat == None:
+            self._ioformat='%d' #Language specific formatting
+        return self._ioformat
+    @ioformat.setter
+    def ioformat(self,value):
+        self._ioformat = value
+
+    @property
+    def definition(self):
+        if self.width==1:
+            self._definition='signal %s :  %s;\n' %(self.name, self.type,)
+        elif not self.init:
+            self._definition='signal %s : %s(%s downto %s);\n' %(self.name, self.type, self.ll, self.rl)
+        else:
+            self._definition='signal %s : %s(%s downto %s) := ;\n' %(self.name, self.type, self.ll, self.rlself.init)
+        return self._definition
+
+    @property
+    def initialization(self):
+        return '%s = %s;\n' %(self.name,self.init)
+    
+    @property
+    def assignment(self,**kwargs):
+        self._assignment='%s <= %s;\n' %(self.name,self.connect.name)
+        return self._assignment
+
+    def nbassign(self,**kwargs):
+        time=kwargs.get('time','')
+        value=kwargs.get('value',self.connect.name)
+        if time:
+            return '%s = #%s %s;\n' %(self.name,time, value)
+        else:
+            return '%s = %s;\n' %(self.name, value)
+
+    def bassign(self):
+        time=kwargs.get('time','')
+        value=kwargs.get('value',self.connect.name)
+        if time:
+            return '%s <= #%s %s;\n' %(self.name,time, value)
+        else:
+            return '%s <= %s;\n' %(self.name, value)
+
+if __name__=="__main__":
+    pass
+
