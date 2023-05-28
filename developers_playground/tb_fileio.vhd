fa820657ca8eb840fb360abab2554f05913a82bc
Marko Kosunen <marko.kosunen@aalto.fi>
2023-05-28 08:34:37 +0300

    Update tb_fileio with show

--- developers_playground/tb_fileio.vhd
+++ developers_playground/tb_fileio.vhd
@@ -9,31 +9,36 @@ entity tb_fileio is
 end tb_fileio;
 
 architecture behavioural of tb_fileio is
+    signal int_in_vect : std_logic_vector(7 downto 0);
+    signal logic_in : std_logic_vector(7 downto 0);
 
 begin
     fileio:process is
     file f_filepath : text open read_mode is filepath;
+    variable v_int_in : integer;
+    variable v_logic_in : logic_in'subtype;
     variable f_line : line;
-    variable int_in : integer;
-    variable logic_in : std_logic_vector(7 downto 0);
     variable read_ok: boolean;
     begin
         report("Starting"); 
         while not endfile(f_filepath) loop
             readline(f_filepath, f_line);
-            read(f_line,int_in,read_ok);
+            read(f_line,v_int_in,read_ok);
             if read_ok then
-                report("Read in integer: " & to_string(int_in) & " which is converted to " & to_string(std_logic_vector(to_signed(int_in,8)))); 
+                report("Read in integer: " & to_string(v_int_in) & " which is converted to " & to_string(std_logic_vector(to_signed(v_int_in,int_in_vect'length)))); 
             else
-                report("Read in integer failed." & to_string(int_in)); 
+                report("Read in integer failed." & to_string(v_int_in)); 
             end if;
+            int_in_vect <= std_logic_vector(to_signed(v_int_in,int_in_vect'length));
             wait for 10 ns;
-            read(f_line,logic_in, read_ok);
+            read(f_line,v_logic_in, read_ok);
             if read_ok then
-                report("Read in logic: " & to_string(logic_in) & " which is converted to " & to_string(to_integer(signed(logic_in))));
+                report("Read in logic: " & to_string(v_logic_in) & " which is converted to " & to_string(to_integer(signed(v_logic_in))));
             else
-                report("Read in logic failed." & to_string(logic_in)); 
+                report("Read in logic failed." & to_string(v_logic_in)); 
             end if;
+            logic_in <= v_logic_in;
+            wait for 10 ns;
         end loop;
         report("Stopping"); 
         wait;
