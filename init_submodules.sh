01c4f83c36b976496a94531538ce9fd42a3e6f1a
Marko Kosunen <marko.kosunen@aalto.fi>
2025-09-04 17:34:05 +0300

    Add pre-commit hooks

---
+++ init_submodules.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#Selective initialization of submodules
+#Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
+DIR=$( cd `dirname $0` && pwd )
+SUBMODULES="\
+"
+if [ -d "${DIR}/.githooks" ]; then
+    git config --local core.hooksPath .githooks/
+fi
+
+git submodule sync
+for mod in $SUBMODULES; do
+    git submodule update --init $mod
+    cd ${mod}
+    if [ -f ./init_submodules.sh ]; then
+        ./init_submodules.sh
+    fi
+    cd ${DIR}
+
+done
+exit 0
+
+
